b647a38781e9fef6ecca743f312ef0db7582ec95
samusesapple <gwansammy@gmail.com>
2024-01-13 20:26:54 +0900

    feat: 프로젝트 파일 생성

---
+++ CountApp/CountApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  CountApp
+//
+//  Created by Sam Sung on 1/13/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
