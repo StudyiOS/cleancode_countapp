86c65a9eb95ab0b967534166dd220dd6544f95c5
dely <lhyal2p@gmail.com>
2024-01-13 16:42:46 +0900

    프로젝트 생성 및 TCA 라이브러리 추가

---
+++ CountApp_Elly/CountApp_Elly/CountApp_EllyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountApp_EllyApp.swift
+//  CountApp_Elly
+//
+//  Created by elly on 1/13/24.
+//
+
+import SwiftUI
+
+@main
+struct CountApp_EllyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
